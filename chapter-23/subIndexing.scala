02e84bae19d4ee2c074b7352e85f642f0feb7f1a
ccarson <ccarson@twitter.com>
2016-12-20 09:45:05 -0800

    added a 3rd layer

--- chapter-23/subIndexing.scala
+++ chapter-23/subIndexing.scala
@@ -1,12 +1,15 @@
 case class Person(name: String, isMale: Boolean, children: Person*)
 
-val lara = Person("Lara", false)
-val bob = Person("Bob", true)
+val gc1 = Person("Brian", true)
+val gc2 = Person("Tina", false)
+val lara = Person("Lara", false, gc1)
+val bob = Person("Bob", true, gc2)
 val julie = Person("Julie", false, lara, bob)
 val persons = List(lara, bob, julie)
 
 //print out a list of mother, child
 for(p <- persons;
     if !p.isMale;
-    c <- p.children)
-yield (p.name, c.name)
+    c <- p.children;
+    gc <- c.children)
+yield (p.name, c.name, gc.name)
